4eca238ec416462e4a5d7fbf79e6b9b3684958d6
KatyR-56 <katy.richardson@nhs.net>
2026-03-03 12:24:46 +0000

    lunchtime

--- recap_lesson.sql
+++ recap_lesson.sql
@@ -17,6 +17,15 @@ WHERE a.[type]='large_airport' --using a. will open up a list of fields in the t
     AND a.[continent]='EU' -- use and to add another criteria for selection
     AND a.[iso_country] IN ('GB','FR')
     -- use in ('criteria 1','criteria 2') to select more than one criteria from a line
-    AND a.latitude_deg between 51 and 54 --use BETWEEN for numbers
+    --AND a.latitude_deg between 51 and 54 --use BETWEEN for numbers
 ORDER BY a.name -- defaults to ascending (ASC), use DESC for descending
 
+-- which 5 regions have the most airports? 
+--use group by to group data by region, then count the number of airports in each region, and order by the count top 5 results only.
+SELECT top 5
+a.iso_region
+    ,COUNT(*) as total_airports
+FROM
+    airports a
+GROUP BY a.iso_region
+ORDER BY total_airports DESC
